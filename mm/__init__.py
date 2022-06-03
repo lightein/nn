1a519b3821c5809e0c7fb0c34b6a364bc2be6319
lightein <xxd0210@163.com>
2022-06-03 19:10:24 +0800

    modify rule of package import

--- mm/__init__.py
+++ mm/__init__.py
@@ -1,4 +1,2 @@
-from .sr.sr_pipline import *
-from .sr.sr_gt_dataset import *
-from .sr.srvggnet import *
-from .sr.srvgg_model import *
+from .sr import *
+from .dns import *
---
+++ mm/sr/__init__.py
@@ -0,0 +1,4 @@
+from .sr_pipline import *
+from .sr_gt_dataset import *
+from .srvggnet import *
+from .srvgg_model import *
